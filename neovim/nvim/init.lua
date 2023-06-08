3abe0a05d0a3d4892fced7519d64a8b3633952a0
SecondSeed <xxmhnu@163.com>
2023-06-08 23:31:22 +0800

    配置vscode neovim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,6 +1,60 @@
 vim.g.mapleader = " "
 
-require('vars')
-require('opts')
-require('keys')
-require('plug')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+},
+{
+  "ggandor/leap.nvim",
+  event = "BufWinEnter",
+  config = function()
+      require('leap').add_default_mappings()
+      vim.api.nvim_set_keymap('n', 'gw', '<Plug>(leap-cross-window)', {})
+      vim.api.nvim_set_keymap('x', 'gw', '<Plug>(leap-cross-window)', {})
+      vim.api.nvim_set_keymap('o', 'gw', '<Plug>(leap-cross-window)', {})
+  end
+},
+{
+  "vim-scripts/ReplaceWithRegister",
+  event = "BufWinEnter",
+},
+{
+  'keaising/im-select.nvim',
+  event = "BufWinEnter",
+  config = function()
+      require('im_select').setup({
+          set_default_events = { "InsertLeave" }
+      })
+  end
+},
+{
+  "mg979/vim-visual-multi",
+  event = "BufWinEnter"
+},
+{
+  "junegunn/vim-easy-align"
+},
+{
+  'vim-scripts/argtextobj.vim'
+}
+})
